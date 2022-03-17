73a72f3299b6a9006a83598564d7006fa38a7a88
basitol <41623944+basitol@users.noreply.github.com>
2022-03-17 01:20:48 +0100

    Did final api call

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const apiUrl = " https://dad-jokes.p.rapidapi.com/random";
-
-fetch(`${apiUrl}"/joke"`, {
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
-    "x-rapidapi-key": "a6dcb74a12mshb549ffdca436b7dp1bb188jsn2d45b58aeb52",
-  },
-})
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    const myJoke = data.body[0].setup;
-    console.log(myJoke);
-  })
-  .catch((err) => console.error(err));
+const quoteText = document.querySelector(".quote");
+const authorName = document.querySelector(".name");
+const quoteBtn = document.querySelector("button");
+const apiUrl = "https://quotes15.p.rapidapi.com/quotes/random/";
 
 const loadJokes = () => {
-  console.log("Hello");
-  document.querySelector(".jokes").innerHTML = "How are you";
+  quoteBtn.innerText = "Loading ...";
+  quoteBtn.classList = "loading";
+
+  fetch(`${apiUrl}`, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "quotes15.p.rapidapi.com",
+      "x-rapidapi-key": "a6dcb74a12mshb549ffdca436b7dp1bb188jsn2d45b58aeb52",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      //   console.log(data.originator.name);
+      quoteText.innerText = data.content;
+      authorName.innerText = data.originator.name;
+      quoteBtn.innerText = "New Quote";
+      quoteBtn.classList.remove("loading");
+    })
+    .catch((err) => console.error(err));
 };
